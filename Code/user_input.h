7e584aebc2c8092f57f360dcb02cd66119e62d6f
tjunruh <tanner.unruh@gmail.com>
2023-05-14 19:06:48 -0500

    user_input.h

---
+++ Code/user_input.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#define ONE 49
+#define TWO 50
+#define THREE 51
+#define FOUR 52
+#define FIVE 53
+#define SIX 54
+#define SEVEN 55
+#define EIGHT 56
+#define NINE 57
+#define S 115
+#define F 102
+#define B 98
+#define H 104
+#define ENTER 13
+#define D 100
+
+#define Q 113
+#define SPACE 32
+
+#ifdef _WIN32
+#define RIGHT 77
+#define LEFT 75
+#define UP 72
+#define DOWN 80
+#elif __linux__
+#define RIGHT 67
+#define LEFT 68
+#define UP 65
+#define DOWN 66
+#endif
