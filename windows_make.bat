a9a1f778bc74d3771c052a14d569a21945b2a796
tjunruh <tanner.unruh@gmail.com>
2024-12-05 22:45:55 -0600

    Create batch file to run stratego on Windows and hide binary and files in bin directory

--- windows_make.bat
+++ windows_make.bat
@@ -1,7 +1,9 @@
 msbuild /m /p:Configuration=Release Code/Stratego.vcxproj
 mkdir build
-mkdir build\board_configs
-copy Code\Release\Stratego.exe build
-copy Code\Release\ascii_engine.dll build
-copy board_configs\* build\board_configs\
+mkdir build\bin
+mkdir build\bin\board_configs
+copy Code\Release\Stratego.exe build\bin\
+copy Code\Release\ascii_engine.dll build\bin\
+copy board_configs\* build\bin\board_configs\
+copy Stratego.bat build\
 rd /s /q Code\Release
