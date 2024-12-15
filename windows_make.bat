604c1fbf81054114437bfae49f4e0c619b3bcd31
tjunruh <tanner.unruh@gmail.com>
2024-12-14 23:22:33 -0600

    Upgrade windows build to Visual Studio 2022 from 2019

--- windows_make.bat
+++ windows_make.bat
@@ -1,9 +1,9 @@
-msbuild /m /p:Configuration=Release Code/Stratego.vcxproj
+msbuild /m /p:Configuration=Release Code/windows_msbuild/Stratego.vcxproj
 mkdir build
 mkdir build\bin
 mkdir build\bin\board_configs
-copy Code\Release\Stratego.exe build\bin\
-copy Code\Release\ascii_engine.dll build\bin\
+copy Code\windows_msbuild\Release\Stratego.exe build\bin\
+copy Code\windows_msbuild\Release\ascii_engine.dll build\bin\
 copy board_configs\* build\bin\board_configs\
 copy Stratego.bat build\
-rd /s /q Code\Release
+FOR /D %%p IN ("Code\windows_msbuild\*.*") DO rmdir "%%p" /s /q
