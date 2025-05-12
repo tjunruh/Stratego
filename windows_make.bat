bbd7e248774b0710f4e0cf23b1fb1d114a70986d
tjunruh <tanner.unruh@gmail.com>
2025-05-11 21:21:39 -0500

    Report error in Windows build

--- windows_make.bat
+++ windows_make.bat
@@ -1,4 +1,8 @@
+status=0
 msbuild /m /p:Configuration=Release Code/windows_msbuild/Stratego.vcxproj
+IF %ERRORLEVEL% NEQ 0 (
+  set status=1
+)
 mkdir build
 mkdir build\bin
 mkdir build\bin\board_configs
@@ -7,3 +11,5 @@ copy Code\windows_msbuild\Release\ascii_engine.dll build\bin\
 copy board_configs\* build\bin\board_configs\
 copy Stratego.bat build\
 FOR /D %%p IN ("Code\windows_msbuild\*.*") DO rmdir "%%p" /s /q
+
+EXIT /b %status%
